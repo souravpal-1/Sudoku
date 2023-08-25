2ab41ab303290121402300fdc9a613821e24ce9c
souravpal-1 <souravpaul7250@gmail.com>
2023-08-26 00:56:01 +0530

    first commit

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ GetPuzzle.onclick = function () {
 		board = response.board
 		FillBoard(board)
 	}
-	xhrRequest.open('get', 'https://sugoku.herokuapp.com/board?difficulty=easy')
-	//we can change the difficulty of the puzzle the allowed values of difficulty are easy, medium, hard and random
+	xhrRequest.open('get', 'https://sugoku.onrender.com/board?difficulty=medium')
+	//we can change the difficulty of the puzzle difficulties are easy, medium, hard and random
 	xhrRequest.send()
 }
 
@@ -43,6 +43,49 @@ SolvePuzzle.onclick = () => {
 	SudokuSolver(board, 0, 0, 9);
 };
 
-function SudokuSolver(board, i, j, n) {
-	// Write your Code here
+
+function isValid(board, i, j, num, n){
+	for(let x = 0; x < n; x++){
+		if(board[i][x] == num || board[x][j] == num){
+			return false;
+		}
+	}
+	let rn = Math.sqrt(n);
+	let si = i - i % rn;
+	let sj = j - j % rn;
+	for(let x = si; x < si + rn; x++){
+		for(let y = sj; y < sj + rn; y++){
+			if(board[x][y] == num){
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+function SudokuSolver(board, i, j, n){
+	if(i == n){
+		FillBoard(board);
+		return true;
+	}
+	if(j == n){
+		return SudokuSolver(board, i+1, 0, n);
+	}
+
+	if(board[i][j] != 0){
+		return SudokuSolver(board, i, j+1, n);
+	}
+
+	for(let num = 1; i <= 9; num++){
+
+		if(isValid(board, i, j, num, n)){
+			board[i][j] = num;
+			let subans = SudokuSolver(board, i, j+1, n);
+			if(subans){
+				return true;
+			}
+			board[i][j] = 0;
+		}
+	}
+	return false;
 }
